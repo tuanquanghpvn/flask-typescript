46a94c41528cb0b4a4d27df9f99f6c3ef1127dfe
Truong Tuan Quang <tuanquanghpvn@gmail.com>
2016-02-02 08:59:14 +0700

    Fesh data random

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 from flask import Flask, render_template, jsonify
+import random
 
 app = Flask(__name__)
 app.config.from_object('app_config')
+
 
 @app.route('/')
 def index():
     return render_template('index.html', body_class="index")
 
+
 @app.route('/json')
 def data():
     data = []
-    for i in xrange(10):
+    for i in xrange(12):
+        my_rand = random.randint(1, 100)
         temp = {
-            'title': 'Hello world ' + str(i),
+            'title': 'Hello student ' + str(my_rand),
             'image': 'default.jpg',
-            'description': 'Hello everyone'
+            'description': 'Your number is: ' + str(my_rand)
         }
         data.append(temp)
     return jsonify(list=data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
